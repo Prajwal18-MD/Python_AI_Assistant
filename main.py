e2e89ab077397c51fb9c9bd7760937f540563df4
Prajwal-18MD <prajju.18gryphon@gmail.com>
2025-04-28 19:07:15 +0530

    Basic Voice Assistant Setup

---
+++ main.py
@@ -0,0 +1,33 @@
+import asyncio
+from dotenv import load_dotenv
+from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
+from livekit.agents.voice_assistant import VoiceAssistant # type: ignore
+from livekit.plugins import openai, silero
+
+load_dotenv()
+
+async def entrypoint(ctx: JobContext):
+    initial_ctx = llm.chat_context().append(
+        role = "system",
+        text = (
+            "You are a VoiceAssistant Created by Livekit. Your Interface with users will be Voice."
+            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation."
+        ),
+    ) 
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+    
+    assistant = VoiceAssistant(
+        vad = silero.VAD.load(),
+        stt = openai.STT(),
+        llm = openai.LLM(),
+        tts = openai.TTS(),
+        chat_ctx = initial_ctx
+    )
+    assistant.start(ctx.room)
+    
+    await asyncio.sleep(1)
+    await assistant.say("Hey , How can I help you today!", allow_interruptions = True)
+
+
+if __name__ == "__main__":
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
